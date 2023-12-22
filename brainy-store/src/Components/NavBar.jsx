bd89f7bd068aab6294667927b88bbbc3c6482f37
OUSSAMA's Pc <lo_boucenna@esi.dz>
2023-12-22 11:07:10 +0100

    tailwindcss added

---
+++ brainy-store/src/Components/NavBar.jsx
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return ( 
+       <nav>
+         <div className="bg-red-700">
+            {//  logo + name 
+            }
+            dkjfk
+         </div>
+         <div className="bg-yellow-600">
+            {/* Home + shop ..  */}
+            jsdhjjdsqfkdsjfkjdh
+         </div>
+         <div className="bg-green-700">
+            {/* LOGIN + CADNA */}
+            dhfjdh
+         </div>
+       </nav>
+     );
+}
+ 
+export default NavBar;
